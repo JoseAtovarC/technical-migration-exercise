b1b5e154d1dbd7b691b95bd9bae90e50feb4ee62
Jose Tovar <86726784+JoseAtovarC@users.noreply.github.com>
2022-07-22 09:52:39 +0200

    feat: http service modified to get more info (#4)

--- src/app/services/pokemons.service.ts
+++ src/app/services/pokemons.service.ts
@@ -7,11 +7,29 @@ import { Observable } from 'rxjs';
 })
 export class PokemonsService {
   private pokeApi = "https://pokeapi.co/api/v2/pokemon/?limit=20"
+  private pokeApiMoreInfo = "https://pokeapi.co/api/v2/pokemon/"
 
   constructor(private http: HttpClient) { }
 
-  public getPokemons(): Promise<any> {
-    return this.http.get(this.pokeApi).toPromise()
+  public async getPokemons(): Promise<any> {
+    let pokeArray: any = []
+    let res: any = await this.http.get(this.pokeApi).toPromise()
+    let pokemons = await res.results.map(async (pokemon: any) => {
+      await this.http.get(this.pokeApiMoreInfo + pokemon.name).toPromise()
+        .then(async (response: any) => {
+
+          pokeArray.push(response)
+        })
+      return pokeArray
+    })
+    await Promise.allSettled(pokemons)
+      .then((d: any) => {
+        pokeArray = d[0].value
+        return pokeArray
+      }
+      )
+
+    return pokeArray
   }
 
 }
