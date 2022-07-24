e588b86947a531e2806e2de557b5e1a5fd11600a
Jose Tovar <86726784+JoseAtovarC@users.noreply.github.com>
2022-07-24 13:21:17 +0200

    Develop (#15)
    * chore: config
    
    * feat: http service
    
    * feat:main-page names in cards
    
    * Feature/card component (#2)
    
    * feat: component card
    
    * feat: name in card component
    
    * fix: flex style in main-page
    
    * fix: name of the component
    
    * Feature/ponke name component (#3)
    
    * feat: create angular js component poke name
    
    * feat: name of the pokemon in agularjs component poke-name
    
    * feat: http service modified to get more info (#4)
    
    * feat: form angular js component (#5)
    
    * Feature/select component (#6)
    
    * feat: angular component select
    
    * feat: select function
    
    * fix: typescript types (#7)
    
    * fix: service pokemons (#8)
    
    * Feature/unit test (#10)
    
    * fix: service pokemons
    
    * fix: sonarcloud suggestions
    
    * feat: new call when http service fail (#12)
    
    * fix:style improvements (#14)

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
-import { poke_data } from 'src/app/interface/poke-data';
+import { PokeData } from 'src/app/interface/poke-data';
 @Component({
   selector: 'card-component',
   templateUrl: './card.component.html',
 
 })
 export class CardComponent {
-  @Input() pokeNames: poke_data[] = [];
+  @Input() pokeNames: PokeData[] = [];
   constructor() { }
 
 
