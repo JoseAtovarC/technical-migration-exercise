701b52037b6e42c970129e9c453699704279244f
Jose Tovar <86726784+JoseAtovarC@users.noreply.github.com>
2022-07-27 03:28:56 +0200

    fix: service functionts and types (#18)

--- src/app/services/pokemons.service.spec.ts
+++ src/app/services/pokemons.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { PokemonsService } from './pokemons.service';
 import { PokeData } from '../../app/interface/poke-data'
-import { lastValueFrom, of } from 'rxjs';
+import { of } from 'rxjs'
 
 
 
@@ -24,6 +24,8 @@ describe('PokemonsService', () => {
     expect(service).toBeTruthy();
   });
 
+  it('should return expected pokemons (HttpClient called once)', (done: DoneFn) => {
 
+  })
 
 });
